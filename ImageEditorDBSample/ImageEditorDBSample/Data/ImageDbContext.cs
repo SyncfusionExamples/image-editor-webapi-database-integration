e630f8f0684604d23a1c0c44395b7a8c72913d3f
Satheeskumar S <satheeskumars@syncfusion.com>
2025-02-10 17:41:58 +0530

    Added sample for Database connection

---
+++ ImageEditorDBSample/ImageEditorDBSample/Data/ImageDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ImageEditorDBSample.Data
+{
+    public class ImageDbContext: DbContext
+    {
+        public ImageDbContext(DbContextOptions<ImageDbContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<CustomImageData> ImageCollections { get; set; }
+
+    }
+}
